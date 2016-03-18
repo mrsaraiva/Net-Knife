289f0d59021079bb100a0e02f89ec3c7155054b9
JoaoLuiz <joaoluiz.si7@gmail.com>
2016-03-18 01:10:25 -0300

    Ping Assincrono.

--- app/src/main/java/matrix/the/net_knife/PingFragment.java
+++ app/src/main/java/matrix/the/net_knife/PingFragment.java
@@ -40,46 +40,6 @@ public class PingFragment extends Fragment{
                 PingTask ping = new PingTask(pingEditText.getText().toString(), pingResultText, getActivity().getApplicationContext());
                 ping.execute();
 
-               // String pingCommand = "ping -c 5";
-               // String hostName = pingEditText.getText().toString();
-               // String cmd = pingCommand + " " + hostName;
-               // pingResultText.setText("Testando o botão antes do ping...");
-
-               // try
-               // {
-               //     System.out.println(cmd);
-               //     Process p = Runtime.getRuntime().exec(cmd);
-               //     p.waitFor();
-
-               //     int len;
-               //     if ((len = p.getErrorStream().available()) > 0)
-               //     {
-               //         System.out.println(p.exitValue());
-               //         byte[] buf = new byte[len];
-               //         p.getErrorStream().read(buf);
-               //         System.out.println("Command error:\t\"" + new String(buf) + "\"");
-               //     }
-
-               //     InputStream input = p.getInputStream();
-               //     System.out.println(input.read());
-               //     BufferedReader in = new BufferedReader(new InputStreamReader(input));
-               //     StringBuffer buffer = new StringBuffer();
-               //     String line = "";
-               //     System.out.println(in.readLine());
-               //     while ((line = in.readLine()) != null)
-               //     {
-               //         System.out.println(in.readLine());
-               //         buffer.append(line);
-               //         buffer.append("\n");
-               //     }
-
-               //     String bufferStr = buffer.toString();
-               //     pingResultText.setText(bufferStr);
-
-               // } catch (Exception e) {
-               //     e.printStackTrace();
-               // }
-
             }
         });
         return view;
